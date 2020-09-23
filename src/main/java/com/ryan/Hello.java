9eaa9ac42dbc45335a062a9d328bce8e637aa7c4
Ryan Lyu <cheng810@gmail.com>
2020-09-23 11:04:13 +0800

    Initial commit

---
+++ src/main/java/com/ryan/Hello.java
@@ -0,0 +1,11 @@
+package com.ryan;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        System.out.println("Hello world2");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/kotlin/com/ryan/Hello.kt
@@ -0,0 +1,15 @@
+package com.ryan
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+
+class Human{
+    fun hello(){
+        println("Hello kotlin in class Human")
+    }
+}
